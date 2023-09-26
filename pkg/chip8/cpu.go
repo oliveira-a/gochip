bd114e05a8584c2a3f1b53415bf9c204a71e6e0b
André Brasil <andremiguelbrasil@gmail.com>
2023-09-26 08:57:09 +0000

    feat(cpu): add clear screen instruction

--- pkg/chip8/cpu.go
+++ pkg/chip8/cpu.go
@@ -2,27 +2,45 @@ package chip8
 
 import (
 	"errors"
+	"fmt"
+)
+
+const (
+	Cols  = 64
+	Rows  = 32
+	Scale = 10
+	Fps   = 60
 )
 
 type CPU struct {
-	screen Drawer
+	drawer  Drawer
+	display [Cols * Rows]uint8
 
 	memory    [4096]uint8
 	registers [16]uint8
 	stack     [16]uint16
 	keys      [8]uint8
 
-	programCounter uint16
-	indexRegister  uint16
-	stackPointer   uint16
-	delayTimer     uint16
-	soundTimer     uint16
+	// The program counter.
+	pc uint16
+
+	// Our index register.
+	ir uint16
+
+	// The stack pointer.
+	sp uint16
+
+	// The delay timer.
+	dt uint16
+
+	// The sound timer.
+	st uint16
 }
 
 func New(d Drawer) *CPU {
 	cpu := &CPU{
-		screen:         d,
-		programCounter: 0x200,
+		drawer: d,
+		pc:     0x200,
 	}
 
 	// Load font into memory - it must start at 0x50
@@ -40,12 +58,73 @@ func (c *CPU) LoadRom(b []byte) error {
 	}
 
 	for i := 0; i < len(b); i++ {
-		c.memory[0x200+1] = b[i]
+		c.memory[c.pc+uint16(i)] = b[i]
 	}
 
 	return nil
 }
 
 func (c *CPU) Run() error {
+	ins := (uint16(c.memory[c.pc]) << 8) | uint16(c.memory[c.pc+1])
+
+	if err := c.exec(ins); err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func (c *CPU) exec(ins uint16) error {
+	opcode := opcode(ins)
+	n := n(ins)
+	nn := nn(ins)
+	nnn := nnn(ins)
+
+	printHex(opcode)
+	printHex(n)
+	printHex(nn)
+	printHex(nnn)
+
+	switch opcode {
+	case 0x0000:
+		switch ins {
+		case 0x00E0:
+			// Clear the display.
+			for i := 0; i < len(c.display); i++ {
+				c.display[i] = 0
+			}
+			break
+		case 0x00EE:
+			c.sp -= 1
+			c.pc = c.stack[c.sp]
+			break
+		}
+	case 0x1000:
+		c.pc = nnn
+		break
+	}
+
+	c.pc += 2
+
+	return nil
+}
+
+func opcode(ins uint16) uint16 {
+	return ins & 0xF000
+}
+
+func n(ins uint16) uint16 {
+	return ins & 0x000F
+}
+
+func nn(ins uint16) uint16 {
+	return ins & 0x00FF
+}
+
+func nnn(ins uint16) uint16 {
+	return ins & 0x0FFF
+}
+
+func printHex(v uint16) {
+	fmt.Printf("%s\n", fmt.Sprintf("%x", v))
 }
--- pkg/chip8/cpu_test.go
+++ pkg/chip8/cpu_test.go
@@ -15,7 +15,7 @@ func TestEmptyRomReturnsError(t *testing.T) {
 func TestProgramCounterStartsAt0x200(t *testing.T) {
 	c8 := New(nil)
 
-	if c8.programCounter != 0x200 {
+	if c8.pc != 0x200 {
 		t.Fail()
 	}
 }
@@ -25,6 +25,22 @@ func TestFontIsLoadedToCorrectMemorySpace(t *testing.T) {
 
 	for i := 0; i < len(font); i++ {
 		if c8.memory[0x50+i] != font[i] {
+			t.Fail()
+		}
+	}
+}
+
+func TestClearsDisplay(t *testing.T) {
+	const ins = 0x00E0
+	c8 := New(nil)
+	for i := 0; i < len(c8.display); i++ {
+		c8.display[i] = 1
+	}
+
+	c8.exec(ins)
+
+	for i := 0; i < len(c8.display); i++ {
+		if c8.display[i] != 0 {
 			t.Fail()
 		}
 	}
