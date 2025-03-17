1252eebb59f0a25252ab486d5e9881d45af8b249
André Brasil <andremiguelbrasil@gmail.com>
2025-03-17 17:06:13 +0000

    fix: spelling

--- server/server.go
+++ server/server.go
@@ -6,10 +6,12 @@ import (
 		"net/http"
 )
 
-func main() {
-	fmt.Println("Listenning on port 8080...")
+const port = ":8080"
 
+func main() {
 	http.Handle("/", http.FileServer(http.Dir(".")))
+
+	fmt.Printf("Listening on port %s...", port)
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
